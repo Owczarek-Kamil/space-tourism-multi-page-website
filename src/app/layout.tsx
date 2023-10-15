e38649fd7cea7e69beaea5387cd61e3c8963d886
Kamil <milkaowca@gmail.com>
2023-10-15 13:23:47 +0200

    Include Google fonts, customize tailwind theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import "@/styles/globals.css";
 import type { Metadata } from "next";
+import { Bellefair, Barlow_Condensed } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Frontend Mentor | Space tourism website",
@@ -12,10 +13,24 @@ export const metadata: Metadata = {
   authors: [{ name: "Kamil Owczarek" }],
 };
 
+const fontBellefair = Bellefair({
+  variable: "--font-bellefair",
+  subsets: ["latin"],
+  weight: "400",
+});
+
+const fontBarlowCondensed = Barlow_Condensed({
+  variable: "--font-barlow-condensed",
+  subsets: ["latin"],
+  weight: "400",
+});
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={""}>{children}</body>
+      <body className={`${fontBellefair.variable} ${fontBarlowCondensed.variable} min-w-[20rem] font-bellefair`}>
+        {children}
+      </body>
     </html>
   );
 }
